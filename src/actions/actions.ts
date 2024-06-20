7ce9f46757f753f209df5ad83ef3973d5db9cc48
Abhimanyu35 <22dcs001@nith.ac.in>
2024-06-21 02:50:21 +0530

    seeding database

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,14 +1,36 @@
-"use server"
-import prisma from "@/lib/db";
+// we will be using this as a server component because all the function that are exported from here must run on the server side 
 
-export async function createPost(formData : FormData){
-      await prisma.post.create({
-        data:{
-          title: formData.get("title") as string,
-          slug: (formData.get("title") as string)
-          .replace(/\s+/g ,"-")
-          .toLowerCase(),
-         content: formData.get("content") as string,
-        }
-      })
+
+"use server"
+
+import prisma from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(formData: FormData){
+  await prisma.post.create({
+     data:{
+         title: formData.get("title") as string,
+         slug: (formData.get("title") as string)
+         .replace(/\s+/g,"-")
+         .toLowerCase(),
+         content:formData.get("content") as string 
+
+     }
+  })
+  // using this function any time this function is called the /posts route will re-render 
+  revalidatePath("/posts");
 }
+
+export async function editPost(formData : FormData , id:string){
+  await prisma.post.update({
+    where: {id},
+    data:{
+      title:formData.get("title") as string,
+      slug:(formData.get("title") as string)
+      .replace(/\s+/g,"-")
+      .toLowerCase(),
+      content : formData.get("content") as string,
+    },
+  })
+}
+
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -4,27 +4,7 @@ import {createPost} from "@/actions/actions";
 import { create } from "domain";
 
 export default async function Page(){
-     const posts = await prisma.post.findMany({
-      where: {
-       title:{
-        contains:"post"
-       },
-      },
-      orderBy:{
-        createdAt: "desc",
-      },
-      select:{
-        id:true,
-        title:true,
-        slug:true,
-
-      },
-      // //for implementing paginatio
-      // take: 1,
-    });
-
-    const postsCount = await prisma.post.count();
-
+     const posts = await prisma.post.findMany();
      return (
       <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
         <h1 className="text-3xl font-semibold">All Posts ({posts.length})</h1>
@@ -35,9 +15,9 @@ export default async function Page(){
             </li>
           ))}
          </ul>
-         //we will be using server actions, now next will take care that when the form is submitted the data gets to the server actions and server
-         
-        <form action={createPost} className="flex flex-col gap-y-2 w-[300px">
+    
+
+        <form action={createPost} className="flex flex-col gap-y-2 w-[300px]">
           <input type="text"
           name="title"
           placeholder="Title"
@@ -47,7 +27,7 @@ export default async function Page(){
           className="px-2 py-1 rounded-sm " />
           <button 
           type="submit"
-          className="bg-blue-500  py-2 text-white rounded-sm">
+          className="bg-blue-500  py-2 text-white      rounded-sm">
             Create a Post
           </button>
            </form>
